34f6aa907fbb4c7b70bc7a513a5e0a9086c63875
Sean Preusse <sppreus@gmail.com>
2025-06-08 21:39:15 +1000

    first

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/turify-landing' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/turify-landing' : '',
   images: {
     unoptimized: true
   }
